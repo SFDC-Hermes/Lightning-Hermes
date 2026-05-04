89cb1cb0d70ee2a66f3144bbf016e13140146b67
SFDC-Hermes <woojin129898@gmail.com>
2026-05-04 09:15:34 +0900

    Update customDatatable.js

--- SFDC-LWC/CustomDatatable/customDatatable.js
+++ SFDC-LWC/CustomDatatable/customDatatable.js
@@ -7,12 +7,17 @@
  * Modifications Log 
  * Ver   Date         Author        Modification
  * 1.0   2026-03-13   SFDC-Hermes   Initial Version
+ * 1.1   2026-05-04   SFDC-Hermes   Add Lookup field type
+
  **/
 import { LightningElement } from 'lwc';
 import LightningDatatable from 'lightning/datatable';
 import picklistEditable from './picklistEditable.html';
 import picklistNotEditable from './picklistNotEditable.html';
 import toggleTemplate from './toggleTemplate.html';
+import lookupTemplate from './lookupTemplate.html';
+import lookupEditTemplate from './lookupEditTemplate.html';
+
 export default class CustomDatatable extends LightningDatatable {
     static customTypes = {
             picklistColumn: {
@@ -25,9 +30,12 @@ export default class CustomDatatable extends LightningDatatable {
                 template:  toggleTemplate,
                 standardCellLayout: true,
                 typeAttributes : ['value', 'context']
-            }
-            
+            },
+            lookup: {
+                template: lookupTemplate,      
+                editTemplate: lookupEditTemplate,
+                standardCellLayout: true,
+                typeAttributes: ['value', 'objectApiName', 'name', 'label', 'context', 'fieldName'] 
+            }            
         };
-       
-       
 }
