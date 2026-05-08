26e1f359f86eaf53799e3c14f4fbc0d31d4d8295
SFDC-Hermes <woojin129898@gmail.com>
2026-05-09 00:08:19 +0900

    master datatable processData

--- SFDC-LWC/MasterDatatable/MasterDatatable.js
+++ SFDC-LWC/MasterDatatable/MasterDatatable.js
@@ -6,23 +6,74 @@ import {LightningElement, api , track, wire } from 'lwc';
 
 export default class MasterDatatable extends LightningElement {
     @api columns;
-    @api objectApiName; 
-    @api picklistFieldApiName; 
+    @api objectApiName;
+    @api picklistFieldApiName;    
+    @api multiPicklistFieldApiName;    
 
-    @track _tableData = []; 
+    @track _tableData = [];
     @track saveDraftValues = [];
     @track pickListOptions;
+    @track multiPickListOptions;
 
     @api
     get tableData() {
         return this._tableData;
     }
-    
     set tableData(value) {
         if (value) {
             this.processData(value);
         } else {
             this._tableData = [];
         }
+    }
+
+    @wire(getObjectInfo, { objectApiName: '$objectApiName' })
+    objectInfo;
+
+    @wire(getPicklistValues, {
+        recordTypeId: '$objectInfo.data.defaultRecordTypeId',
+        fieldApiName: '$picklistFieldApiName'
+    })
+    wirePickList({ error, data }) {
+        if (data) {
+            this.pickListOptions = data.values;
+            if (this._tableData.length > 0) {
+                this.processData(this._tableData);
+            }
+        } else if (error) {
+            console.error('Picklist load error:', error);
+        }
+    }
+
+    @wire(getPicklistValues, {
+        recordTypeId: '$objectInfo.data.defaultRecordTypeId',
+        fieldApiName: '$multiPicklistFieldApiName'
+    })
+    wireMultiPickList({ error, data }) {
+        if (data) {
+            this.multiPickListOptions = data.values;
+            if (this._tableData.length > 0) {
+                this.processData(this._tableData);
+            }
+        } else if (error) {
+            console.error('Multi-picklist load error:', error);
+        }
+    }
+
+    processData(dataArray) {
+        this._tableData = dataArray.map(row => {
+            const newRow = { ...row };
+
+            for (const key in row) {
+                if (row[key] && typeof row[key] === 'object' && row[key].Name) {
+                    const lookupIdField = key.endsWith('__r') ? key.replace('__r', '__c') : key + 'Id';
+                    newRow[`${lookupIdField}_Name`] = row[key].Name;
+                }
+            }
+
+            newRow.pickListOptions = this.pickListOptions;
+            newRow.multiPickListOptions = this.multiPickListOptions;
+            return newRow;
+        });
     }
 }
