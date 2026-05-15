35ea7147b8be36ce7ea93e4976d64534f3935199
SFDC-Hermes <woojin129898@gmail.com>
2026-05-15 21:00:14 +0900

    update lwc code

---
+++ SFDC-LWC/Datatable/Datatable.js
@@ -0,0 +1,16 @@
+/**
+ * @description       : 
+ * @author            : SFDC-Hermes
+ * @group             : 
+ * @last modified on  : 2026-05-15
+ * @last modified by  : SFDC-Hermes
+ * Modifications Log 
+ * Ver   Date         Author        Modification
+ * 1.0   2026-05-15   SFDC-Hermes   Initial Version
+
+ **/
+import { LightningElement, track, api } from 'lwc';
+
+export default class Datatable extends LightningElement {
+
+}
