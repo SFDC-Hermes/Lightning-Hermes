48051a2b201af4a064383858f48615d4b958e909
SFDC-Hermes <woojin129898@gmail.com>
2026-05-16 19:34:19 +0900

    update dt js

--- SFDC-LWC/Datatable/Datatable.js
+++ SFDC-LWC/Datatable/Datatable.js
@@ -9,8 +9,55 @@
  * 1.0   2026-05-15   SFDC-Hermes   Initial Version
 
  **/
-import { LightningElement, track, api } from 'lwc';
+import getRecordName from '@salesforce/apex/COMM_Utility.getRecordName';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { updateRecord } from 'lightning/uiRecordApi';
+import {LightningElement, api , track, wire } from 'lwc';
 
 export default class Datatable extends LightningElement {
+    @api columns;
+    @api objectApiName;
 
+    @track _tableData = [];
+    @track saveDraftValues = [];
+
+    @api
+    get tableData() {
+        return this._tableData;
+    }
+    set tableData(value) {
+        if (value && value.length > 0) {
+            this._rawData = JSON.parse(JSON.stringify(value)); 
+            this.processData(this._rawData);
+        } else {
+            this._rawData = [];
+            this._tableData = [];
+        }
+    }
+
+    @wire(getObjectInfo, { objectApiName: '$objectApiName' })
+    objectInfo;
+
+    processData(dataArray) {
+        this._tableData = dataArray.map(row => {
+            const newRow = { ...row };
+            
+            for (const key in row) {
+                if (row[key] && typeof row[key] === 'object' && row[key].Name) {
+                    const lookupIdField = key.endsWith('__r') ? key.replace('__r', '__c') : key + 'Id';
+                    newRow[`${lookupIdField}_Name`] = row[key].Name;
+                }
+            }
+
+            return newRow;
+        });
+    }
+
+    refreshTableData() {
+    if (!this._rawData || this._rawData.length === 0) {
+        return;
+    }
+    this.processData(this._rawData); 
+    }
 }
